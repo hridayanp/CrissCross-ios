e556a070b66153b0cfb3b49e88fa63944e4ab761
hridayanp <hridayanphukan123@gmail.com>
2025-03-28 14:12:29 +0530

    Initial Commit

---
+++ CrissCross/CrissCrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrissCrossApp.swift
+//  CrissCross
+//
+//  Created by Hridayan Phukan on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CrissCrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrissCrossTests/CrissCrossTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrissCrossTests.swift
+//  CrissCrossTests
+//
+//  Created by Hridayan Phukan on 28/03/25.
+//
+
+import Testing
+@testable import CrissCross
+
+struct CrissCrossTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
